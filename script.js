4a2d565ad2462961e747884919a046f792e2e500
alexandrafs5 <alexandra051009@gmail.com>
2024-08-27 18:31:05 -0700

    add the point function

--- script.js
+++ script.js
@@ -1,33 +1,71 @@
-let numberOneChosen = 7;
-let numberTwoChosen = 5;
-let operatorChosen = "+";
+let previousValue = "";
+let currentValue = "";
+let operatorChosen = "";
+let result = "";
 
-function add(numberOne, numberTwo) {
-    return numberOne + numberTwo;
-}
+const dot = document.querySelector(".dot");
+const clear = document.querySelector(".clear");
+const erase = document.querySelector(".erase");
+const equals = document.querySelector(".result");
+const numbers = document.querySelectorAll(".numbers");
+const currentScreen = document.querySelector(".current");
+const previousScreen = document.querySelector(".previous");
+const operators = document.querySelectorAll(".operators");
 
-function subtract(numberOne, numberTwo) {
-    return numberOne - numberTwo;
-}
+numbers.forEach((number) => number.addEventListener("click", function(e){
+    handleNumber(e.target.textContent);
+    currentScreen.textContent = currentValue;
+}))
 
-function multiply(numberOne, numberTwo) {
-    return numberOne * numberTwo;
-}
+operators.forEach((operator) => operator.addEventListener("click", function(e){
+    handleOperator(e.target.textContent);
+    currentScreen.textContent = "";
+    previousScreen.textContent = previousValue + " " + operatorChosen;
+}))
 
-function divide(numberOne, numberTwo) {
-    return numberOne / numberTwo;
-}
+equals.addEventListener("click", function() {
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
 
-function operate(operator) {
-    if (operator == "+") {
-        console.log(add(numberOneChosen, numberTwoChosen));
-    } else if (operator == "-") {
-        console.log(subtract(numberOneChosen, numberTwoChosen));
-    } else if (operator == "*") {
-        console.log(multiply(numberOneChosen, numberTwoChosen));
-    } else if (operator == "/") {
-        console.log(divide(numberOneChosen, numberTwoChosen));
+    if (operatorChosen === "+") {
+        result = previousValue + currentValue;
+    } else if (operatorChosen === "-") {
+        result = previousValue - currentValue;
+    } else if (operatorChosen === "*") {
+        result = previousValue * currentValue;
+    } else if (operatorChosen === "/") {
+        result = previousValue / currentValue;
+    } else if (operatorChosen === "%") {
+        result = previousValue % currentValue;
+    }
+
+    previousScreen.textContent = previousValue + operatorChosen + currentValue + "=";
+    currentScreen.textContent = result;
+    previousValue = result;
+
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
+})
+
+dot.addEventListener("click", function(e){
+    handlePoint(e.target.textContent)
+    currentScreen.textContent = currentValue;
+})
+
+function handleNumber(number) {
+    if (currentValue.length <= 16) {
+        currentValue += number;
     }
 }
 
-operate(operatorChosen);
+function handleOperator(operator) {
+    operatorChosen = operator;
+    previousValue = currentValue;
+    currentValue = "";
+}
+
+function handlePoint() {
+    if (currentValue.length <= 16) {
+        currentValue += ".";
+    }
+}
